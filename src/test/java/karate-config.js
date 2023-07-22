532820ed88692045fde668832d1cf365c1d266e2
Krishan Kumawat <krishan.kumawat@thinkvibes.com>
2023-07-22 21:27:04 +0530

    content-type commit

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,5 +1,6 @@
 function fn() {
   karate.configure('connectTimeout', 5000);
+  karate.configure('readTimeout', 5000);
   var env = karate.env; // get system property 'karate.env'
   karate.log('karate.env system property was:', env);
   if (!env) {
